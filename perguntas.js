f247cc09ed509e468063452240133dcb0a898901
wesleyy7 <142313571+wesleyy7@users.noreply.github.com>
2024-11-14 07:28:47 -0300

    14/11

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'CRISTIANO RONALDO',
+    'Quantos gols Cristiano Ronaldo tem na carreira?',
+    '907'
+)
+criaCartao(
+    'CRISTIANO RONALDO',
+    'Quantos títulos de Champions League CR7 tem?',
+    '5'
+)
+criaCartao(
+    'CRISTIANO RONALDO',
+    'Quantos gols Cristiano marcou pela UCL?',
+    '141'
+)
